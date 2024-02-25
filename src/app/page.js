92e2815c19394d587f950c7ec302f4a168a2feaa
joshgollaher <joshg@mopine.com>
2024-02-24 21:45:06 -0600

    added cool lines

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,46 @@
+"use client";
 import Image from "next/image";
 import { WavyBackground } from "@/components/WavyBackground";
+import { GoogleGeminiEffect } from "@/components/GeminiEffect";
+import { useState, useEffect } from "react";
+import { useTransform, useMotionValue } from 'framer-motion'
 
 export default function Home() {
+
+  const [forwards, setForwards] = useState(true);
+  const progress = useMotionValue(0);
+  const maxProgress = 1;
+  const rate = 0.04;
+
+  const length = useMotionValue(0.2);
+
+  const off1 = useMotionValue(Math.random() / 3);
+  const off2 = useMotionValue(Math.random() / 3);
+  const off3 = useMotionValue(Math.random() / 3);
+  const off4 = useMotionValue(Math.random() / 3);
+  const off5 = useMotionValue(Math.random() / 3);
+
+  useEffect(() => {
+
+    function update() {
+      progress.set(progress.get() + (1 / 60 * rate * (forwards ? 1 : -1)));
+      requestAnimationFrame(update);
+    }
+
+    update();
+
+  }, []);
+
   return (
-    <>
-      <WavyBackground>
-        <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
-          Josh & Braden Braces LLC
-        </p>
-        <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
-          Coming Q1 2025
-        </p>
-      </WavyBackground>
-    </>
+    <div
+      className="h-screen bg-black w-full dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip"
+    >
+      <GoogleGeminiEffect
+        pathLengths={[progress, progress, progress, progress, progress]}
+        pathOffsets={[
+          progress, progress, progress, progress, progress
+        ]} title="Josh & Braden Braces LLC" description="Coming Q1 2025">
+      </GoogleGeminiEffect>
+    </div>
   );
 }
