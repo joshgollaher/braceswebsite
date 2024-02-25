2e8aa4a1519e3e8b2ece6f44531a9af00a93d267
joshgollaher <joshg@mopine.com>
2024-02-25 02:33:03 -0600

    add button

--- src/components/GeminiEffect.tsx
+++ src/components/GeminiEffect.tsx
@@ -26,12 +26,20 @@ export const GoogleGeminiEffect = ({
       <p className="text-lg md:text-7xl font-normal pb-4 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-300">
         {title || `Build with Aceternity UI`}
       </p>
-      <p className="text-xs md:text-xl font-normal text-center text-neutral-400 mt-4 max-w-lg mx-auto">
-        {description ||
-          `Scroll this component and see the bottom SVG come to life wow this
+      <div className="flex flex-col text-center">
+        <p className="text-xs md:text-xl font-normal text-center text-neutral-400 mt-4 max-w-lg mx-auto">
+          {description ||
+            `Scroll this component and see the bottom SVG come to life wow this
         works!`}
-      </p>
-      <div className="w-full h-[890px] -top-60 md:-top-40  flex items-center justify-center bg-red-transparent absolute ">
+        </p>
+        <div className="mx-auto m-4">
+          <button className="w-40 relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+              Whitepaper
+            </span>
+          </button>
+        </div>
       </div>
       <svg
         width="1440"
